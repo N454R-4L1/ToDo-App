e571664c20af9b870c0f2cc669cd138453389de6
N454R-4L1 <11.ali.nasar@gmail.com>
2024-07-15 19:43:43 +0530

    New Changes

--- app.py
+++ app.py
@@ -72,5 +72,5 @@ def about():
     return render_template('./dashboard/about.html')
     
 
-if (__name__) == '__main__':
-    app.run(debug=True)
+# if (__name__) == '__main__':
+#     app.run(debug=True)
